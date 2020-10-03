5c82df252018467c9e5305520ffc5fc2c33e8f49
chamilaadhi <cka.adhi@gmail.com>
2020-10-03 06:28:22 +0530

    add for tenant

--- access-token.sh
+++ access-token.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 username=admin
+#username=admin@wso2.com
 tokenUser=admin
-appname=setup_apim_script
+appname=setup_apim_script_1
 client_request() {
     cat <<EOF
 {
@@ -17,6 +18,7 @@ EOF
 client_credentials=$(curl -k -u $username:admin -H "Content-Type: application/json" -d "$(client_request)" https://localhost:9443/client-registration/v0.14/register| jq -r '.clientId + ":" + .clientSecret')
 #echo $client_credentials
 get_access_token() {
+	echo "curl -k -d \"grant_type=password&username=$tokenUser&password=admin&scope=apim:api_view apim:api_publish apim:api_create apim:subscribe\" -u $client_credentials https://localhost:8243/token\n"
     local access_token=$(curl -k -d "grant_type=password&username=$tokenUser&password=admin&scope=apim:api_view apim:api_publish apim:api_create apim:subscribe" -u $client_credentials https://localhost:8243/token | jq -r '.access_token')
     echo $access_token
 }
