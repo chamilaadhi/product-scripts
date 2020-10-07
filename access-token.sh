932f079cc2fb9d38413e71918f851bd83780b331
kasunboy <72492184+kasunboy@users.noreply.github.com>
2020-10-07 13:49:38 +0530

    add new scopes to the token gen call

--- access-token.sh
+++ access-token.sh
@@ -18,8 +18,8 @@ EOF
 client_credentials=$(curl -k -u $username:admin -H "Content-Type: application/json" -d "$(client_request)" https://localhost:9443/client-registration/v0.17/register| jq -r '.clientId + ":" + .clientSecret')
 #echo $client_credentials
 get_access_token() {
-	echo "curl -k -d \"grant_type=password&username=$tokenUser&password=admin&scope=apim:api_view apim:api_publish apim:api_create apim:subscribe\" -u $client_credentials https://localhost:8243/token\n"
-    local access_token=$(curl -k -d "grant_type=password&username=$tokenUser&password=admin&scope=apim:api_view apim:api_publish apim:api_create apim:subscribe" -u $client_credentials https://localhost:8243/token | jq -r '.access_token')
+	echo "curl -k -d \"grant_type=password&username=$tokenUser&password=admin&scope=apim:api_workflow_view apim:api_workflow_approve apim:tenantInfo apim:admin_settings apim:api_view apim:api_publish apim:api_create apim:subscribe\" -u $client_credentials https://localhost:8243/token\n"
+    local access_token=$(curl -k -d "grant_type=password&username=$tokenUser&password=admin&scope=apim:api_workflow_view apim:api_workflow_approve apim:tenantInfo apim:admin_settings apim:api_view apim:api_publish apim:api_create apim:subscribe" -u $client_credentials https://localhost:8243/token | jq -r '.access_token')
     echo $access_token
 }
 
