e9591d756d0f99d1e381e8395091bd68243c5956
Chamila Adhikarinayake <cka.adhi@gmail.com>
2022-10-27 15:30:40 +0530

    Remove log

--- add-swagger.sh
+++ add-swagger.sh
@@ -6,6 +6,6 @@ echo "\nAccess token : " $access_token "\n"
 product_id=$(cat id.productid)
 echo "Product Id : " $product_id
 
-echo "\n"curl -k -H \"Authorization: Bearer $access_token\" -F apiDefinition=@openapi.json -X PUT https://localhost:9443/api/am/publisher/v1.0/api-products/$product_id/swagger
+#echo "\n"curl -k -H \"Authorization: Bearer $access_token\" -F apiDefinition=@openapi.json -X PUT https://localhost:9443/api/am/publisher/v1.0/api-products/$product_id/swagger
 payload=$(curl -k -H "Authorization: Bearer $access_token" -F apiDefinition=@openapi.json -X PUT https://localhost:9443/api/am/publisher/v1.0/api-products/$product_id/swagger)
 echo "\n\nAPI Product swagger: \n\n" $payload "\n\n"
